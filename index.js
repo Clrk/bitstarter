8947c093296fa9e1cf47b60cb34e7de00f365b8c
Tibo <thibault.rouveyrol@gmail.Com>
2014-09-18 23:26:39 +0000

    edited index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express')
 var app = express();
+var str = fr.readFile('index.html');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2, le nouveau retour!')
+  response.send(str)
 })
 
 app.listen(app.get('port'), function() {
