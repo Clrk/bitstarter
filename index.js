c144d22a84056930d77e7cc044c9c822846b7340
Tibo <thibault.rouveyrol@gmail.Com>
2014-09-19 00:03:12 +0000

    edited index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-var express = require('express')
+var fs = require ('fs');
+var express = require('express');
 var app = express();
-var str = fr.readFile('index.html');
+var html = fs.readFile('index.html');
+
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(str)
+  response.send(html.toString())
 })
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+(__dirname + '/index.html');
